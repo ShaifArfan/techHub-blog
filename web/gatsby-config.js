49a07ba10dd962e61f479391c13128b78f981914
arfan <shaifullaharfan17@gmail.com>
2022-02-02 07:26:26 +0600

    ✅initial commit

---
+++ web/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+      title: `techHub`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-sass"]
+};
