6113390befd23791d03dda011efeb97171cea58e
arfan <shaifullaharfan17@gmail.com>
2022-04-07 02:17:30 +0600

    post-rec-fix

--- web/src/components/author/AuthorItem.js
+++ web/src/components/author/AuthorItem.js
@@ -1,11 +1,9 @@
-import { Link } from 'gatsby';
 import React from 'react';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import ParagraphText from '../typography/ParagraphText';
 import { AuthorItemStyles } from '../../styles/author/AuthorItemStyles';
 import { Title } from '../typography/Title';
 
-function AuthorItem({ name, description, slug, profileImage }) {
+function AuthorItem({ name, slug, profileImage }) {
   return (
     <AuthorItemStyles className="author-item" to={slug.current}>
       <GatsbyImage
